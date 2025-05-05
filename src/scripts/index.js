bbb24ca4ffe037e91095e7fd106db8eddbf716f3
Dulyadech <dulyadech.p@kkumail.com>
2025-05-05 20:53:04 +0700

    sofia

---
+++ src/scripts/index.js
@@ -0,0 +1,12 @@
+const inputSearch = document.getElementById("search");
+const iconSearch = document.getElementById("iconSearch");
+
+iconSearch.addEventListener("click", () => {
+  inputSearch.style.opacity = "1";
+  inputSearch.style.width = "100%";
+});
+
+inputSearch.addEventListener("blur", () => {
+  inputSearch.style.opacity = "0";
+  inputSearch.style.width = "0";
+});
